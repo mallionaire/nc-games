821632cb659750e42afb467910aa35a2554f574e
mallionaire <hjmallion@gmail.com>
2021-01-21 17:00:59 +0000

    added public/html with routes information for hosting

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const e = require('express');
-const express = require('express');
+const express = require("express");
 const app = express();
-const apiRouter = require('./routers/api.router');
-const { handles500s, handlesInvalidPaths, handleCustomErrors, handlePSQLErrors } = require('./errors/errors');
+const apiRouter = require("./routers/api.router");
+const {
+  handles500s,
+  handlesInvalidPaths,
+  handleCustomErrors,
+  handlePSQLErrors,
+} = require("./errors/errors");
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.all('/*', handlesInvalidPaths);
+app.all("/*", handlesInvalidPaths);
 
-app.use(handleCustomErrors)
+app.use(handleCustomErrors);
 
-app.use(handlePSQLErrors)
+app.use(handlePSQLErrors);
 
 app.use(handles500s);
 
