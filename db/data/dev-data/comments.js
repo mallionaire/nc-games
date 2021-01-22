9da520275721d24fd1ccae740021655722ce6416
Philippa Bushell <philippabushell@Philippas-MacBook-Pro.local>
2021-01-22 12:12:09 +0000

    add more dev data

--- db/data/dev-data/comments.js
+++ db/data/dev-data/comments.js
@@ -1,46 +1,148 @@
 module.exports = [
   {
-    body: "I loved this game too!",
-    belongs_to: "Jenga",
-    created_by: "happyamy2016",
+    body: 'I loved this game too!',
+    belongs_to: 'JengARRGGGH',
+    created_by: 'happyamy2016',
     votes: 16,
     created_at: 1511354163389,
   },
   {
-    body: "My dog loved this game too!",
-    belongs_to: "Ultimate Werewolf",
-    created_by: "tickle122",
+    body: 'My dog loved this game too!',
+    belongs_to: 'Ultimate Werewolf',
+    created_by: 'tickle122',
     votes: 3,
     created_at: 1610964545410,
   },
   {
     body: "I didn't know dogs could play games",
-    belongs_to: "Ultimate Werewolf",
-    created_by: "weegembump",
+    belongs_to: 'Ultimate Werewolf',
+    created_by: 'weegembump',
     votes: 10,
     created_at: 1610964588110,
   },
   {
-    body: "EPIC board game!",
-    belongs_to: "Jenga",
-    created_by: "happyamy2016",
+    body: 'EPIC board game!',
+    belongs_to: 'JengARRGGGH',
+    created_by: 'tickle122',
     votes: 16,
     created_at: 1511354163389,
   },
   {
     body:
-      "Now this is a story all about how, board games turned my life upside down",
-    belongs_to: "Jenga",
-    created_by: "cooljmessy",
+      'Now this is a story all about how, board games turned my life upside down',
+    belongs_to: 'tickle122',
+    created_by: 'cooljmessy',
     votes: 13,
     created_at: 1610965445410,
   },
   {
     body:
-      "Not sure about dogs, but my cat likes to get involved with board games, the boxes are their particular favourite",
-    belongs_to: "Ultimate Werewolf",
-    created_by: "happyamy2016",
+      'Not sure about dogs, but my cat likes to get involved with board games, the boxes are their particular favourite',
+    belongs_to: 'Ultimate Werewolf',
+    created_by: 'happyamy2016',
     votes: 10,
     created_at: 1616874588110,
   },
+  {
+    body: 'Quis duis mollit ad enim deserunt.',
+    belongs_to: "Scythe; you're gonna need a bigger table!",
+    created_by: 'jessjelly',
+    votes: 3,
+    created_at: 1616874538110,
+  },
+  {
+    body: 'Laboris nostrud ea ex occaecat aute quis consectetur anim.',
+    belongs_to: "That's just what an evil person would say!",
+    created_by: 'cooljmessy',
+    votes: 17,
+    created_at: 1616854538110,
+  },
+  {
+    body:
+      'Consequat nisi dolor nulla esse sunt eu ipsum laborum deserunt duis.',
+    belongs_to: 'Build you own tour de Yorkshire',
+    created_by: 'weegembump',
+    votes: 1,
+    created_at: 1616854536110,
+  },
+  {
+    body:
+      'Ex id ipsum dolore non cillum anim sint duis nisi anim deserunt nisi minim.',
+    belongs_to: 'JengARRGGGH!',
+    created_by: 'grumpy19',
+    votes: 9,
+    created_at: 1616854531110,
+  },
+  {
+    body:
+      'Commodo aliquip sunt commodo elit in esse velit laborum cupidatat anim.',
+    belongs_to: 'One Night Ultimate Werewolf',
+    created_by: 'happyamy2016',
+    votes: 10,
+    created_at: 1616854521110,
+  },
+  {
+    body: 'Aliquip aliqua ad fugiat anim ex elit consectetur ut fugiat ex qui.',
+    belongs_to: 'A truly Quacking Game; Quacks of Quedlinburg',
+    created_by: 'tickle122',
+    votes: 8,
+    created_at: 1616854551110,
+  },
+  {
+    body:
+      'Sint elit ut ex aliquip laboris in elit Lorem elit incididunt cillum fugiat.',
+    belongs_to: 'Build you own tour de Yorkshire',
+    created_by: 'weegembump',
+    votes: 8,
+    created_at: 161684559110,
+  },
+  {
+    body: 'Commodo et non ut aute anim nisi occaecat ea veniam ut ad.',
+    belongs_to: "That's just what an evil person would say!",
+    created_by: 'jessjelly',
+    votes: 15,
+    created_at: 1611313922734,
+  },
+  {
+    body: 'Ex aliqua irure elit aliqua in veniam ex ut pariatur nulla.',
+    belongs_to: 'Dolor reprehenderit',
+    created_by: 'tickle122',
+    votes: 1,
+    created_at: 1630010368077,
+  },
+  {
+    body:
+      'Duis anim aliqua consequat mollit ullamco fugiat qui mollit minim proident.',
+    belongs_to: "Settlers of Catan: Don't Settle For Less",
+    created_by: 'cooljmessy',
+    votes: 2,
+    created_at: 1611316192508,
+  },
+  {
+    body:
+      'Nostrud adipisicing reprehenderit adipisicing velit do do quis do veniam ut adipisicing aliqua do reprehenderit.',
+    belongs_to: "Settlers of Catan: Don't Settle For Less",
+    created_by: 'weegembump',
+    votes: 10,
+    created_at: 1611316224771,
+  },
+  {
+    body:
+      'Reprehenderit ipsum eiusmod sint amet veniam ex veniam cillum quis et Lorem mollit dolor ipsum.',
+    belongs_to: 'Occaecat consequat officia in quis commodo.',
+    created_by: 'grumpy19',
+    votes: 7,
+    created_at: 1620010368077,
+  },
 ];
+
+// Scythe; you're gonna need a bigger table!
+// "That's just what an evil person would say!"
+// "Build you own tour de Yorkshire"
+// "A truly Quacking Game; Quacks of Quedlinburg"
+// "One Night Ultimate Werewolf"
+// "JengARRGGGH!"
+// 'Dolor reprehenderit'
+//"Settlers of Catan: Don't Settle For Less"
+//'Proident tempor et.'
+// 'Occaecat consequat officia in quis commodo.'
