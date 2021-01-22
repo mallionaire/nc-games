209f18f4a5ec8eb039b2961a0d7507d8b745f5f4
mallionaire <hjmallion@gmail.com>
2021-01-22 10:16:32 +0000

    added scripts and production key for hosting

--- db/data/index.js
+++ db/data/index.js
@@ -1,10 +1,13 @@
 const ENV = process.env.NODE_ENV || 'development'
 
 const test = require("./test-data")
+const development = require('./dev-data')
 
 
 const data = {
-  test: test 
+  test, 
+  development, 
+  production: development 
 }
 
 module.exports = data[ENV]
