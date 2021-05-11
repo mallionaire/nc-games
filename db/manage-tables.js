87a41b12b1507b698a3d3a12a3c4d0e54991714d
P-Copley <pkcopley@gmail.com>
2021-05-11 13:02:19 +0100

    replace migrations with manage-tables functions

---
+++ db/manage-tables.js
@@ -0,0 +1,50 @@
+const db = require('./connection');
+
+const createTables = async () => {
+  const categoriesTablePromise = db.query(`
+  CREATE TABLE categories (
+    slug VARCHAR PRIMARY KEY,
+    description VARCHAR
+  );`);
+
+  const usersTablePromise = db.query(`
+  CREATE TABLE users (
+    username VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    avatar_url VARCHAR
+  );`);
+
+  await Promise.all([categoriesTablePromise, usersTablePromise]);
+
+  await db.query(`
+  CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    category VARCHAR NOT NULL REFERENCES topics(slug),
+    owner VARCHAR NOT NULL REFERENCES users(username),
+    review_body TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    votes INT DEFAULT 0 NOT NULL,
+    designer VARCHAR NOT NULL,
+    review_img_url TEXT NOT NULL
+  );`);
+
+  await db.query(`
+  CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    body TEXT NOT NULL,
+    review_id INT REFERENCES reviews(review_id) NOT NULL ON DELETE CASCADE,
+    author VARCHAR REFERENCES users(username) NOT NULL,
+    votes INT DEFAULT 0 NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+  );`);
+};
+
+const dropTables = async () => {
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS reviews;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS categories;`);
+};
+
+module.exports = { createTables, dropTables };
